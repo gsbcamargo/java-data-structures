b3d0212f0c8a31337c54b09485891f5d20c20591
Gabriel Camargo <gsbcamargo@protonmail.com>
2026-07-07 01:13:28 -0300

    creates basic graph edge vertex

---
+++ src/com/gabriel/datastructures/graph/Edge.java
@@ -0,0 +1,26 @@
+package com.gabriel.datastructures.graph;
+
+public class Edge {
+
+    private final Vertex start;
+    private final Vertex end;
+    private final Integer weight;
+
+    public Edge(Vertex startV, Vertex endV, Integer inputWeight) {
+        this.start = startV;
+        this.end = endV;
+        this.weight = inputWeight;
+    }
+
+    public Vertex getStart() {
+        return start;
+    }
+
+    public Vertex getEnd() {
+        return end;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+}
---
+++ src/com/gabriel/datastructures/graph/Graph.java
@@ -0,0 +1,5 @@
+package com.gabriel.datastructures.graph;
+
+public class Graph {
+
+}
